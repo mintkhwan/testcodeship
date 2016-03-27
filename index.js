83869075b248d7a98ef481e99448ee6bce60c610
Mint <mskylinee@hotmail.com>
2016-03-27 14:53:21 +0700

    test sum

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /* global describe,it */
-var num1 = 5
-var num2 = 5
-function sum (num1, num2) {
+var num1 = 1
+var num2 = 2
+function plus (num1, num2) {
   return num1 + num2
 }
-console.log(sum(num1, num2))
+console.log(plus(num1, num2))
 
 var assert = require('assert')
 describe('check sum function', function () {
-  describe('sum()', function () {
+  describe('plus()', function () {
     it('should return 3', function () {
-      assert.equal(sum(num1, num2), 10)
+      assert.equal(plus(num1, num2), 3)
     })
   })
 })
