3e6aef37ec1f014adce8be739ce81908ec0e2c48
Mint <mskylinee@hotmail.com>
2016-03-27 14:47:50 +0700

    sum

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-// var num1 = 1
-// var num2 = 2
+/* global describe,it */
+var num1 = 1
+var num2 = 2
 function plus (num1, num2) {
   return num1 + num2
 }
+console.log(plus(num1, num2))
+
+var assert = require('assert')
+describe('check sum function', function () {
+  describe('plus()', function () {
+    it('should return 3', function () {
+      assert.equal(plus(num1, num2), 3)
+    })
+  })
+})
